fb31f331f7185204f3a0f2923cbca93415abb25b
Harshit Nanda <harshitnanda25@gmail.com>
2026-04-30 17:19:04 +0530

    array

---
+++ D.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class D {
+    public static void main (String args []){
+        // int[] marks = new int [3];
+        // marks [0] = 90; // chemistry
+        // marks [1] = 91; // maths
+        // marks [2] = 95; // physics
+        // // System.out.println (marks [0]);
+        // // System.out.println (marks [1]);
+        // // System.out.println (marks [2]);
+
+        // for (int i=0; i<3; i++){
+        //     System.out.println(marks [i]);
+        // }
+        Scanner sc = new Scanner (System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int [size];
+        for (int i=0; i<size; i++){
+            System.out.println(numbers [i]);
+        }
+    }
+    
+}
+
