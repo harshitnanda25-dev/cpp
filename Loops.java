6dfb25ba32934027a3f68ddc7cdc219db1bbae87
Harshit Nanda <harshitnanda25@gmail.com>
2026-05-05 03:05:14 +0530

    sum of n

---
+++ Loops.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class  Loops {
+    public static void main(String args []) {
+     
+    //     // Sum of (n) numbers //
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int sum = 0;
+    //  for (int i=1; i<=a ; i++){
+    //      sum = sum + i;
+    //  }
+    // System.out.println(sum);
+
+
+    // Table of any numbers // 
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+     
+    for ( int i = 1; i <= 10 ; i++){
+        System.out.print(n*i + " ");
+    }
+    }  
+}
